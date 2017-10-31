e43db77152bfefc0d8d4f2cf0815dc7fdb536ecc
Matthew D. Scholefield <matthew3311999@gmail.com>
2017-10-31 13:53:44 -0500

    Wait for wakeword and responses in logs

--- test-upgrade.sh
+++ test-upgrade.sh
@@ -5,22 +5,6 @@ log_dir="./logs"
 num_failed=0
 num_succeeded=0
 
-# Checks if the binary executable exists in the PATH
-# Usage: found_exe EXE_NAME
-found_exe() {
-    hash "$1" 2>/dev/null
-}
-
-setup_colors() {
-    if found_exe tput; then
-        green="$(tput setaf 2; tput bold)"
-        red="$(tput setaf 1; tput bold)"
-        white="$(tput setaf 7; tput bold)"
-
-        reset="$(tput sgr0)"
-    fi
-}
-
 # Runs a command, tracking the status of success or failure
 # Usage: run CMD_NAME CMD_ARGS
 run() {
@@ -45,16 +29,16 @@ run() {
     echo "$log"
 
     if [ "$code" != "0" ]; then
-        echo ":: ${red}FAILED$reset to run $white$cmd_name$reset"
+        echo ":: FAILED to run $cmd_name"
         if [ -n "$log" ]; then
             echo
-            echo "${white}Output:$reset $log"
+            echo "Output: $log"
             echo
         fi
         num_failed=$((num_failed + 1))
     else
         num_succeeded=$((num_succeeded + 1))
-        echo ":: ${green}SUCCESS$reset running $white$cmd_name$reset"
+        echo ":: SUCCESS running $cmd_name"
     fi
 }
 
@@ -102,18 +86,27 @@ reset_logs() {
     done
 }
 
-# Usage: is_in_logs REGEX
-is_in_logs() {
-    regex="$1"
-    cat $log_dir/mycroft-* | grep "$regex"
-}
-
 # Send a message to the mycroft messagebus
 # Usage: send_message MSG DATA_JSON
 send_message() {
     msg="$1"
     data="${2:-\{\}}"
     python -c "from threading import Thread; from mycroft.messagebus.client.ws import WebsocketClient; from mycroft import Message; from time import sleep; ws = WebsocketClient(); Thread(target=ws.run_forever).start(); sleep(0.2); ws.emit(Message('$msg', data=$data)); ws.close()"
+}
+
+# Waits $3 seconds for $1 to appear in $2 log
+# Usage: wait_for_str_in_log STRING LOG_NAME [TIMEOUT]
+wait_for_str_in_log() {
+    str="$1"
+    log_name="$2"
+    timeout="${3-10}"
+    for i in $(seq 1 0.1 "$timeout"); do
+        if grep -q "$str" "$log_dir/$log_name"; then
+            return 0 
+        fi
+        sleep 0.1
+    done
+    return 1
 }
 
 # Checks if the query causes the output in the logs
@@ -124,14 +117,12 @@ check_behavior() {
 
     reset_logs
     send_message 'recognizer_loop:utterance' '{"utterances": ["'"$1"'"]}'
-    sleep 2
-    is_in_logs "$2"
+    wait_for_str_in_log "$2" mycroft-*.log
 }
 
 set -e
 
 mkdir -p logs
-#setup_colors
 
 if [ "$1" != "-s" ]; then
     run setup_keys
@@ -143,10 +134,12 @@ if [ "$1" != "-s" ]; then
     check_processes dead
 
     run sudo apt-get install -y mycroft-mark-1
+    sleep 3
     check_processes
 
     run set_repo unstable
     run sudo apt-get install -y mycroft-mark-1
+    sleep 3
     check_processes
 else
     shift
@@ -154,12 +147,14 @@ fi
 
 log_dir="${1:-$log_dir}"
 
+run wait_for_str_in_log "Waiting for wake word" mycroft-voice.log
+
 run check_behavior "what is the weather" "a high of"
 run check_behavior "what time is it" "[AP]M"
 
 echo
-[ "$num_failed" -gt "0" ] && echo   "${red}   Failed${reset}: $num_failed"
-[ "$num_succeeded" -gt "0" ] && echo "${green}Succeeded${reset}: $num_succeeded"
+[ "$num_failed" -gt "0" ] && echo   "$   Failed: $num_failed"
+[ "$num_succeeded" -gt "0" ] && echo "Succeeded: $num_succeeded"
 echo
 
 [ "$num_failed" = 0 ]
